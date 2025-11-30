a65c052a08a1edb4f454ec4df43ca2998a274779
Raisul akram <akramraisul4@gmail.com>
2025-11-30 20:01:48 +0700

    Pelanggan

--- src/Pelanggan.java
+++ src/Pelanggan.java
@@ -1,1 +1,37 @@
+public class Pelanggan {
+    private int idTiket;
+    private String namaLengkap;
+    private int usia;
+    private boolean isHamil;
+    private int sisaSaldo;
+    private Kategori kategori;
 
+    public Pelanggan(int id, String nama, int usia, boolean hamil) {
+        this.idTiket = id;
+        this.namaLengkap = nama;
+        this.usia = usia;
+        this.isHamil = hamil;
+        this.sisaSaldo = 10000; 
+        this.kategori = tentukanKategori();
+    }
+
+    private Kategori tentukanKategori() {
+        if (usia > 60) return Kategori.LANSIA;
+        if (usia < 10) return Kategori.ANAK_ANAK;
+        if (isHamil) return Kategori.IBU_HAMIL;
+        
+        // Logika Remaja (10 - 19 tahun)
+        if (usia >= 10 && usia <= 20) return Kategori.REMAJA; 
+        
+        return Kategori.UMUM;
+    }
+    public int getId() { return idTiket; }
+    
+    public String getNama() { return namaLengkap; }
+    public int getSaldo() { return sisaSaldo; }
+    public Kategori getKategori() { return kategori; }
+
+    public void bayarTiket(int harga) {
+        this.sisaSaldo -= harga;
+    }
+}
